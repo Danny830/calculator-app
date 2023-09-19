9293d74e30be62bb85f5c867df95ca626a90f8d3
Danny830 <danmilanes@gmail.com>
2023-09-19 17:26:10 -0400

    Added project files
    Current functionalities
    - Add, Subtract, divide and multiply all functional and working
    - Percent, decimal, all clear and equal all work as intended
    - Buttons widgets and number show properly on screen
    - Result number won't break the screen (number gets compressed, still need fixing)

---
+++ lib/main.dart
@@ -0,0 +1,574 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Calculator App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  num _result = 0;
+  List<num> list = [];
+  List<num> query = [];
+  String _stringResult = "0";
+  String resultList = "0";
+  String queryResult = "";
+  String listResult = "";
+  bool _override = true;
+  bool addition = false;
+  bool subtraction = false;
+  bool multiplication = false;
+  bool division = false;
+  bool equalState = false;
+  bool decimalState = false;
+  bool negation = false;
+
+  void _incrementCounter() {
+    setState(() {
+      _result++;
+    });
+  }
+
+  void clearArithmetic() {
+
+    addition = false;
+    subtraction = false;
+    multiplication = false;
+    division = false;
+    equalState = false;
+    resultList = "0";
+
+  }
+
+  void _input(input) {
+
+    if (equalState) {
+
+      equalState = false;
+      list.clear();
+      query.clear();
+
+    }
+
+    if (resultList.contains('.')) {
+
+      resultList = resultList + input;
+      list.clear();
+      list.add(num.parse(resultList));
+      setState(() {
+        _result = num.parse(resultList);
+        _stringResult = resultList;
+      });
+      print(_stringResult);
+
+    }
+    else {
+
+      list.add(num.parse(input));
+      setState(() {
+        _result = num.parse(list.join());
+        _stringResult = _result.toString();
+        resultList = _result.toString();
+      });
+      print(_result);
+
+    }
+
+  }
+
+  void _giveResult(String arithmetic) {
+
+    listResult = list.join();
+    queryResult = query.join();
+    print(queryResult + " And " + listResult);
+
+    switch(arithmetic) {
+      case "add":
+        setState(() {
+          _result = num.parse(queryResult) + num.parse(listResult);
+          _stringResult = _result.toString();
+        });
+        query.clear();
+        query.add(_result);
+        break;
+      case "sub":
+        setState(() {
+          _result = num.parse(queryResult) - num.parse(listResult);
+          _stringResult = _result.toString();
+        });
+        query.clear();
+        query.add(_result);
+        break;
+      case "mul":
+        setState(() {
+          _result = num.parse(queryResult) * num.parse(listResult);
+          _stringResult = _result.toString();
+        });
+        query.clear();
+        query.add(_result);
+        break;
+      case "div":
+        try {
+          setState(() {
+            if (num.parse(queryResult) % num.parse(listResult) == 0) {
+              _result = num.parse(queryResult) ~/ num.parse(listResult);
+            }
+            else {
+              _result = num.parse(queryResult) / num.parse(listResult);
+            }
+            _stringResult = _result.toString();
+          });
+          query.clear();
+          query.add(_result);
+          print("No division error");
+        }
+        catch(e) {
+          print("Error caught, Divide by Zero");
+          setState(() {
+            _stringResult = "Error";
+          });
+          query.clear();
+          list.clear();
+        }
+        break;
+    }
+
+  }
+
+  void _allClear() {
+    setState(() {
+      _result = 0;
+      resultList = "0";
+      _stringResult = "0";
+      list.clear();
+      query.clear();
+      _override = true;
+      addition = false;
+      subtraction = false;
+      multiplication = false;
+      division = false;
+      equalState = false;
+      decimalState = false;
+    });
+  }
+
+  void _negate() {
+
+    // if (query.isEmpty) {
+    //   setState(() {
+    //     _result = _result * -1;
+    //     _stringResult = _result.toString();
+    //     resultList = _stringResult;
+    //   });
+    //   list.clear();
+    //   list.add(_result);
+    // }
+    // else {
+    //   setState(() {
+    //     _result = _result * -1;
+    //     _stringResult = _result.toString();
+    //     resultList = _stringResult;
+    //   });
+    //   query.clear();
+    //   query.add(_result);
+    // }
+
+
+  }
+
+  void _percent() {
+
+    // If the query list is same as the result, then we update
+    // the query instead of the list. This is so we keep track
+    // of the number that we keep manipulating when doing arithmetic
+    // or when using the percent on our result after pressing equals
+    if (query.join() == _result.toString()) {
+      _result = _result / 100;
+      query.clear();
+      query.add(_result);
+      setState(() {
+        _stringResult = _result.toString();
+      });
+      print("query");
+      print(query);
+    }
+    else {
+      _result = _result / 100;
+      list.clear();
+      list.add(_result);
+      setState(() {
+        _stringResult = _result.toString();
+      });
+      print("list");
+      print(list);
+    }
+
+  }
+
+  void _addition() {
+
+    // If query is empty, no arithmetic should be performed and we set
+    // our query to the result of our list
+    if (query.isEmpty) {
+      query = List.from(list);
+    }
+    // We check if the user just hit the "equal" sign so we don't perform
+    // more arithmetic than needed and check if any arithmetic should
+    // be completed before moving on
+    else if ((subtraction || multiplication || division) && !equalState) {
+      _equal();
+    }
+    // We run a continuous addition result if the user keeps pressing "+"
+    else {
+
+      try {
+        continuousArithmetic("add");
+      }
+      catch (e) {
+        print("No number input");
+      }
+
+    }
+    // Disable all other arithmetic flags, set the correct flag to true and
+    // clear the list for the next user input
+    clearArithmetic();
+    addition = true;
+    list.clear();
+    print(query);
+    print("Addition True");
+
+  }
+
+  void _subtraction() {
+
+    if (query.isEmpty) {
+      query = List.from(list);
+    }
+    else if ((addition || multiplication || division) && !equalState) {
+      _equal();
+    }
+    else {
+
+      try {
+        continuousArithmetic("sub");
+      }
+      catch (e) {
+        print("No number input");
+      }
+
+    }
+    clearArithmetic();
+    subtraction = true;
+    list.clear();
+    print(query);
+    print("Subtraction True");
+
+  }
+
+  void _multiplication() {
+
+    if (query.isEmpty) {
+      query = List.from(list);
+    }
+    else if ((addition || subtraction || division) && !equalState) {
+      _equal();
+    }
+    else {
+
+      try {
+        continuousArithmetic("mul");
+      }
+      catch (e) {
+        print("No number input");
+      }
+
+    }
+    clearArithmetic();
+    multiplication = true;
+    list.clear();
+    print(query);
+    print("Multiplication True");
+
+  }
+
+  void _division() {
+
+    if (query.isEmpty) {
+      query = List.from(list);
+    }
+    else if ((addition || multiplication || subtraction) && !equalState) {
+      _equal();
+    }
+    else {
+
+      try {
+        continuousArithmetic("div");
+      }
+      catch (e) {
+        print("No number input");
+      }
+
+    }
+    clearArithmetic();
+    division = true;
+    list.clear();
+    print(query);
+    print("Division True");
+
+  }
+
+  void continuousArithmetic(String arith) {
+
+    if (equalState) {
+      list.clear();
+    }
+    else {
+      _giveResult(arith);
+    }
+
+  }
+
+  void _equal() {
+
+    if (list.isEmpty) {
+      list = List.from(query);
+    }
+
+    if (addition) {
+      _giveResult("add");
+    }
+    else if (subtraction) {
+      _giveResult("sub");
+    }
+    else if (multiplication) {
+      _giveResult("mul");
+    }
+    else if (division) {
+      _giveResult("div");
+    }
+
+  }
+
+  void decimal() {
+
+    if (!resultList.contains(".")) {
+      setState(() {
+        resultList = "$resultList.";
+        _stringResult = resultList;
+      });
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          heightSeparation(),
+          Expanded (
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: Text(
+                  _stringResult,
+                  style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              button("AC"),
+              widthSeparation(),
+              button("+/-"),
+              widthSeparation(),
+              button("%"),
+              widthSeparation(),
+              button("/"),
+            ],
+          ),
+          heightSeparation(),
+          Row(
+            children: [
+              button("7"),
+              widthSeparation(),
+              button("8"),
+              widthSeparation(),
+              button("9"),
+              widthSeparation(),
+              button("X"),
+            ],
+          ),
+          heightSeparation(),
+          Row(
+            children: [
+              button("4"),
+              widthSeparation(),
+              button("5"),
+              widthSeparation(),
+              button("6"),
+              widthSeparation(),
+              button("-"),
+            ],
+          ),
+          heightSeparation(),
+          Row(
+            children: [
+              button("1"),
+              widthSeparation(),
+              button("2"),
+              widthSeparation(),
+              button("3"),
+              widthSeparation(),
+              button("+"),
+            ],
+          ),
+          heightSeparation(),
+          Row(
+            children: [
+              button("0"),
+              widthSeparation(),
+              button("."),
+              widthSeparation(),
+              button("="),
+            ],
+          ),
+        ],
+      )
+    );
+  }
+
+  Widget widthSeparation() {
+
+    return SizedBox(width: 5);
+
+  }
+
+  Widget heightSeparation() {
+
+    return SizedBox(height: 5);
+
+  }
+
+  Widget button(String input) {
+
+    if (input == "0") {
+
+      return SizedBox(
+        width: 200,
+        height: 120,
+        child: ElevatedButton(
+            onPressed: () {
+              _input("0");
+            },
+            child: Text(input,
+                style: TextStyle(fontSize: 35))),
+      );
+
+    }
+    else {
+
+      return SizedBox(
+        width: 99,
+        height: 120,
+        child: ElevatedButton(
+            onPressed: () {
+              switch(input) {
+                case "AC":
+                  _allClear();
+                  break;
+                case "+/-":
+                  _negate();
+                  break;
+                case "%":
+                  _percent();
+                  break;
+                case "1":
+                  _input(input);
+                  break;
+                case "2":
+                  _input(input);
+                  break;
+                case "3":
+                  _input(input);
+                  break;
+                case "4":
+                  _input(input);
+                  break;
+                case "5":
+                  _input(input);
+                  break;
+                case "6":
+                  _input(input);
+                  break;
+                case "7":
+                  _input(input);
+                  break;
+                case "8":
+                  _input(input);
+                  break;
+                case "9":
+                  _input(input);
+                  break;
+                case "+":
+                  _addition();
+                  break;
+                case "-":
+                  _subtraction();
+                  break;
+                case "X":
+                  _multiplication();
+                  break;
+                case "/":
+                  _division();
+                  break;
+                case "=":
+                  equalState = true;
+                  _equal();
+                  break;
+                case ".":
+                  decimal();
+                  break;
+
+              }
+
+            },
+            child: Text(input,
+                style: TextStyle(fontSize: 35))),
+      );
+
+    }
+
+  }
+}
